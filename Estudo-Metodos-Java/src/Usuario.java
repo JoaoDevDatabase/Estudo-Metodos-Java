9cca2d2dc23213b56eb026a1a3c143e3999dbdb6
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-12 14:14:11 -0300

    Implementação de novos códigos na TV SmartTv

--- Estudo-Metodos-Java/src/Usuario.java
+++ Estudo-Metodos-Java/src/Usuario.java
@@ -1,24 +1,36 @@
 public class Usuario {
     public static void main(String[] args) {
-
+        // acumulando o arquivo na memoria
         SmartTv smarttv = new SmartTv();
 
+        // inicio de meu programa
+        System.out.println("\n" + "/**********SmartTVSimulator**********/\n");
         System.out.println("TELEVISÃO LIGADA? : " + smarttv.ligartv);
         System.out.println("CANAL ? " + smarttv.canal);
         System.out.println("VOLUME : " + smarttv.volume);
 
+        // aumentando o volume da televisão
         smarttv.aumentarVolume();
         smarttv.aumentarVolume();
         smarttv.aumentarVolume();
         smarttv.aumentarVolume();
         smarttv.aumentarVolume();
-        System.out.printf("Aumentando o volume de 25, para : %d%n", +smarttv.volume);
+        System.out.printf("Aumentando o volume de 25, para : %d%n \n", +smarttv.volume);
+
+        // mudando de canal
         System.out.println("Deseja mudar o canal ? ");
+        smarttv.passarCanal();
+        smarttv.passarCanal();
+        smarttv.passarCanal();
+        System.out.println("Mudando o canal, para o canal : " + smarttv.canal + "\n");
 
-        smarttv.passarCanal();
-        smarttv.passarCanal();
-        smarttv.passarCanal();
-        System.out.println("Mudando o canal, para o canal : " + smarttv.canal);
+        // voltando para o canal principal de origem
+        System.out.println("Deseja voltar o canal para o canal padrão?");
+        smarttv.voltarCanal();
+        smarttv.voltarCanal();
+        smarttv.voltarCanal();
+        System.out.println("CANAL ATUAL : " + smarttv.canal + "\n");
 
+        // FIM DE MEU PROGRAMA
     }
 }
